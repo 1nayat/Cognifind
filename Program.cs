a6ad91ca0f4e925618ccbdeb1a0d584f78708cb8
1nayat <inayatw086@gmail.com>
2026-02-17 11:26:05 +0530

    fixed issues

--- Program.cs
+++ Program.cs
@@ -99,5 +99,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://*:{port}");
 
 app.Run();
